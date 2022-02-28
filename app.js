91af7977abe4199e0278f67f1278e0b29dd65302
chsdwn <chasedown@hotmail.com>
2022-02-28 18:53:09 +0300

    Installs express-fileupload. Stores uploaded photos in public/uploads folder. Renders photos sorted by dateCreated descending. Renders photo images on index and photo views. Adds enctype multipart/form-data to the add photo form.

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express')
+const fileupload = require('express-fileupload')
+const fs = require('fs')
 const mongoose = require('mongoose')
+const path = require('path')
 const Photo = require('./models/photo')
 
 mongoose.connect('mongodb://localhost/pcat')
@@ -16,10 +19,11 @@ const pathNameLogger = (req, res, next) => {
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(fileupload())
 app.use(pathNameLogger)
 
 app.get('/', async (req, res) => {
-  const photos = await Photo.find({})
+  const photos = await Photo.find({}).sort('-dateCreated')
   res.render('index', { photos })
 })
 app.get('/about', (req, res) => {
@@ -28,9 +32,17 @@ app.get('/about', (req, res) => {
 app.get('/add', (req, res) => {
   res.render('add')
 })
-app.post('/photos', async (req, res) => {
-  await Photo.create(req.body)
-  res.redirect('/')
+app.post('/photos', (req, res) => {
+  const uploadDir = path.join('public', 'uploads')
+  if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir)
+
+  const uploadedImage = req.files.image
+  const uploadPath = path.join(__dirname, 'public', 'uploads', uploadedImage.name)
+
+  uploadedImage.mv(uploadPath, async () => {
+    await Photo.create({ ...req.body, image: '/uploads/' + uploadedImage.name })
+    res.redirect('/')
+  })
 })
 app.get('/photos/:id', async (req, res) => {
   const photo = await Photo.findById(req.params.id)
